6a911990fa0a843cfb3ab1956c7fa176c5ec7e06
Eythan David <eythan.cvt@gmail.com>
2021-12-06 11:47:54 -0500

    feat: footer

---
+++ src/components/layouts/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div>footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
