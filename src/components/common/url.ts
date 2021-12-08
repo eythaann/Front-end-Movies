cb1946345e4713fd900129c5bbb90b34297fbf88
Eythan David <eythan.cvt@gmail.com>
2021-12-08 17:25:00 -0500

    build: final!

--- src/components/common/url.ts
+++ src/components/common/url.ts
@@ -1,6 +1,8 @@
-const port = process.env.PORT || 4000;
-const url = `http://localhost:${port}/api/v1`;
+const url = "https://localhost:4000/api/v1";
+//URL PROD "https://app-59a524a1-f591-4c88-ab7b-bee461635357.cleverapps.io/api/v1"
+//URL DEV  "https://localhost:4000/api/v1"
 
-export const publicFolder = `http://localhost:${port}`;
-
+export const publicFolder = `https://localhost:4000`;
+//URL PROD "http://app-59a524a1-f591-4c88-ab7b-bee461635357.cleverapps.io"
+//URL DEV "https://localhost:4000"
 export default url;
